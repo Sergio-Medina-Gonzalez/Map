c79bb6b608a3303ae5a6b6904895fce21ece8d71
Dr. Sergio Medina-González <102832113+Sergio-Medina-Gonzalez@users.noreply.github.com>
2022-04-01 14:58:06 -0700

    Update appmap.py

--- appmap.py
+++ appmap.py
@@ -4,4 +4,5 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 df2= pd.read_csv('https://raw.githubusercontent.com/Sergio-Medina-Gonzalez/Map/main/data.csv')
 #st.title('My map')
-print df
+st.map(df)
+st.map(df2)
